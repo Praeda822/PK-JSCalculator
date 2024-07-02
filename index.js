75724be43df9aff5d022deb37d2332c29260226e
Patrick Kelly <patdkelly5@gmail.com>
2024-07-02 19:53:11 +1000

    Final working edition of the calculator

--- index.js
+++ index.js
@@ -2,8 +2,18 @@
 
 const display = document.getElementById("display");
 
-function appendToDisplay(input) {}
+function appendToDisplay(input) {
+  display.value += input;
+}
 
-function clearDisplay() {}
+function clearDisplay() {
+  display.value = "";
+}
 
-function calculate() {}
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (err) {
+    display.value = "Error";
+  }
+}
