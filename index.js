90ba3d8d2bfaf6b6eb40e9f98f962317e83257e7
Patrick Kelly <patdkelly5@gmail.com>
2024-07-02 22:07:18 +1000

    Refactored for modularity using ES6 classes

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
 "use strict";
 
-const display = document.getElementById("display");
+class Calculator {
+  constructor(displayElementId) {
+    this.displayElement = document.getElementById(displayElementId);
+    this.init();
+  }
 
-function appendToDisplay(input) {
-  display.value += input;
-}
+  appendToDisplay(input) {
+    this.displayElement.value += input;
+  }
 
-function clearDisplay() {
-  display.value = "";
-}
+  clearDisplay() {
+    this.displayElement.value = "";
+  }
 
-function calculate() {
-  try {
-    display.value = eval(display.value);
-  } catch (err) {
-    display.value = "Error";
+  calculate() {
+    try {
+      this.displayElement.value = eval(this.displayElement.value);
+    } catch (err) {
+      this.displayElement.value = "Error";
+    }
+  }
+
+  init() {
+    document.querySelectorAll(".number-btn").forEach((button) => {
+      button.addEventListener("click", () =>
+        this.appendToDisplay(button.textContent)
+      );
+    });
+
+    document.querySelectorAll(".operator-btn").forEach((button) => {
+      button.addEventListener("click", () =>
+        this.appendToDisplay(button.textContent)
+      );
+    });
+
+    document
+      .getElementById("equals-btn")
+      .addEventListener("click", () => this.calculate());
+    document
+      .getElementById("clear-btn")
+      .addEventListener("click", () => this.clearDisplay());
   }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  new Calculator("display");
+});
